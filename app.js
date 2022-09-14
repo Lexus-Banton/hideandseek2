4464a8355a435326647625a870efab9b40b646c9
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-13 18:29:50 -0700

    set state

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 /* Imports */
+const rockButton = document.getElementById('rock-button');
+const paperButton = document.getElementById('paper-button');
+const cutButton = document.getElementById('cut-button');
+
+const rockContainer = document.getElementById('rock-container');
+const paperContainer = document.getElementById('paper-container');
+const cutContainer = document.getElementById('cut-container');
+
+const winsEl = document.getElementById('wins');
+const totalEl = document.getElementById('total');
+const lossesEl = document.getElementById('losses');
 
 /* State */
+const handSigns = ['rock', 'Paper', 'cut'];
+
+let correctGusses = 0;
+let totalGusses = 0;
+
+//set event listeners
+
+rockButton;
 
 /* Actions */
 function loadPage() {}
